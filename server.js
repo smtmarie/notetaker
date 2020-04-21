02e792be1e7f9ab6952fa49c3d50c63cc401255f
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-04-21 16:30:59 -0600

    initial work

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require ("express");
+const apiroutes = require("./routes/apiroutes");
+const htmlroutes = require("./routes/htmlroutes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use()
+app.use()
+app.use()
+app.use()
+app.use()
