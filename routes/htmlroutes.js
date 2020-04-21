345dd7454a6593a153f24923817b64842a0ba3dc
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-04-21 17:48:48 -0600

    updates

---
+++ routes/htmlroutes.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const router = require("express").Router();
+
+router.length("/notes", function(req, res) {
+
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+router.length("*", function(req, res) {
+
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const htmlroutes = require("./routes/htmlroutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use()
-app.use()
-app.use()
-app.use()
-app.use()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiroutes);
+app.use("/", htmlroutes);
+
+app.listen(PORT, ()=> console.log (PORT));
