d4c0f50fcefbc4acb5bee972080f674bac364daf
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-04-21 19:07:53 -0600

    more work

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const router = require("express").Router();
 
-router.length("/notes", function(req, res) {
+router.get("/notes", function(req, res) {
 
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-router.length("*", function(req, res) {
+router.get("*", function(req, res) {
 
     res.sendFile(path.join(__dirname, "../public/index.html"));
 
