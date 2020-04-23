061f954516ea0bfc426f35d916206fb34ba47b97
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-04-23 16:26:28 -0600

    <heroku update>

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -6,7 +6,7 @@ router.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-router.get("*", function(req, res) {
+router.get("/", function(req, res) {
 
     res.sendFile(path.join(__dirname, "../public/index.html"));
 
